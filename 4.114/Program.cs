093cb32fd6b671847bacb080d050ba0cc310f369
polog <polog@DESKTOP-JCMULE7>
2022-10-24 21:48:10 +0300

    Добавьте файлы проекта.

---
+++ 4.114/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Год: ");
+int year = int.Parse(Console.ReadLine());
+ Console.WriteLine("Номер месяца: ");
+int mounth = int.Parse(Console.ReadLine());
+Console.WriteLine("День: ");
+int day = int.Parse(Console.ReadLine());
+Console.WriteLine($"Дата предыдущего дня: {day - 1}.{mounth}.{year}");
+Console.WriteLine($"Дата следующего дня: {day + 1}.{mounth}.{year}");
+  if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+ {
+                Console.WriteLine("Год високосный");
+}
+  else
+{
+                Console.WriteLine("Год не високосный");
+}
